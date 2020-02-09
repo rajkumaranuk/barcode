c6b0ec1a31a60b4e72c97523dc7d9b152b4ce29a
Raj Kumaran <raj.kumaranuk@gmail.com>
2020-02-09 22:52:42 +0000

    Move URL into variable in tests

--- src/test/java/com/royalmail/barcode/BarcodeApplicationTests.java
+++ src/test/java/com/royalmail/barcode/BarcodeApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -20,51 +21,53 @@ class BarcodeApplicationTests {
     @LocalServerPort
     private int port;
 
+    private static final String URL = "http://localhost:%d/validate/%s";
+
     @Test
     void validateBarcodeWithValidFormatReturnsTrue() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/AB473124829GB", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, "AB473124829GB"), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody().isValid()).isEqualTo(true);
     }
 
     @Test
     void validateBarcodeWithValidPaddedSerialNumberReturnsTrue() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/AB000000014GB", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, "AB000000014GB"), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody().isValid()).isEqualTo(true);
     }
 
     @Test
     void validateBarcodeWithInvalidCheckDigitReturnsFalse() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/AB473124828GB", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, "AB473124828GB"), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody().isValid()).isEqualTo(false);
     }
 
     @Test
     void validateBarcodeWithInvalidPrefixReturnsFalse() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/a473124829GB", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, "a473124829GB"), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody().isValid()).isEqualTo(false);
     }
 
     @Test
     void validateBarcodeWithInvalidSerialNumberReturnsFalse() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/AB124829GB", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, "AB124829GB"), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody().isValid()).isEqualTo(false);
     }
 
     @Test
     void validateBarcodeWithInvalidCountryCodeReturnsFalse() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/AB473124829EN", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, "AB473124829EN"), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody().isValid()).isEqualTo(false);
     }
 
     @Test
     void validateEmptyBarcodeReturnsNotFound() {
-        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity("http://localhost:" + port + "/validate/", BooleanValue.class);
+        ResponseEntity<BooleanValue> entity = restTemplate.getForEntity(format(URL, port, ""), BooleanValue.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
